8766169ba81632f6845544dd130599edab0b094d
Maxime TFT <mkubik@tftlabs.com>
2022-06-02 17:01:55 +0200

    [💎 Layout] Add the simplest layout to build a fullscreen webapp using flex and flex-1

---
+++ components/layout.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Layout({ children }) {
+  const router = useRouter();
+
+  const menuItems = [
+    {
+      href: '/',
+      title: 'Homepage',
+    },
+    {
+      href: '/about',
+      title: 'About',
+    },
+    {
+      href: '/contact',
+      title: 'Contact',
+    },
+  ];
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-emerald-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase">
+        💎 Next.js Fullscreen
+      </header>
+      <div className="flex flex-col md:flex-row flex-1">
+        <aside className="bg-emerald-100 w-full md:w-60">
+          <nav>
+            <ul>
+              {menuItems.map(({ href, title }) => (
+                <li className="m-2" key={title}>
+                  <Link href={href}>
+                    <a
+                      className={`flex p-2 bg-emerald-200 rounded hover:bg-emerald-400 cursor-pointer ${
+                        router.asPath === href && 'bg-emerald-600 text-white'
+                      }`}
+                    >
+                      {title}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </aside>
+        <main className="flex-1">{children}</main>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex h-full flex-col justify-center items-center">
+      <h1 className="text-4xl mb-5 font-bold">Homepage</h1>
+      <span className="text-7xl">♻</span>
+    </div>
+  );
+}
