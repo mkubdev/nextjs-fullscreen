12800a5e429ffcd3e67f87755f8e5aed71d4f334
Max Kubik <50140834+mkubdev@users.noreply.github.com>
2022-06-02 18:05:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-full flex-col justify-center items-center">
+    <div className="flex h-full flex-col justify-center items-center p-4 bg-sky-100">
       <h1 className="text-4xl mb-5 font-bold">Homepage</h1>
       <span className="text-7xl">♻</span>
     </div>
