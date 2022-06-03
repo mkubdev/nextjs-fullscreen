05cf9af2854c5152f46d37487bfc335c33474f31
Max Kubik <50140834+mkubdev@users.noreply.github.com>
2022-06-03 17:37:17 +0200

    [fix] fix #2: the page not overlap the footer now

--- pages/graph.js
+++ pages/graph.js
@@ -2,7 +2,7 @@ import Line2 from "../components/Line2";
 
 export default function Contact() {
   return (
-    <div className="h-full m-4">
+    <div className="m-4">
       <Line2 />
     </div>
   );
